b28798c1a840f0577a70f1fa63e27571666624af
jqwang <jqwang2009@163.com>
2023-03-28 16:34:48 +0800

    预加载脚本

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 // 使用 CommonJS 导入两个 Electron 模块
 // app - 控制应用的事件生命周期
 // BrowserWindow - 负责创建和管理应用的窗口
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, ipcMain} = require('electron')
+const path = require('path')
 
 const createWindow = () => {
     const win = new BrowserWindow(
         {
             width: 800,
             height: 600,
+            autoHideMenuBar: true,
+            webPreferences: {
+                preload: path.join(__dirname, 'preload.js')
+            },
         }
     )
+
+    ipcMain.handle('ping', () => 'pong')
 
     win.loadFile('index.html')
 }
